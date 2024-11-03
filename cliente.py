19ea1aec62e0a50dbeba8e970d61518108eed9e3
Kaua Hen <115280997+KauaHenSilva@users.noreply.github.com>
2024-11-02 21:38:25 -0300

    feat: connect to cliente

--- cliente.py
+++ cliente.py
@@ -3,74 +3,79 @@ import sys
 
 SERVER_POST = 9000
 BUFFER = 1024
-ADDRESS = '127.0.0.1'
+ADDRESS = "127.0.0.1"
+
 
 class Cliente:
-    
-    def enviar_mensagem(self,address):
+
+    def enviar_mensagem(self, address):
         while True:
             username = input("Username: ")
-            if username != '':
+            if username != "":
                 mensagem = input("Voce: ")
-                
-                if mensagem != '':
-                    mensagem = f'{username}, {mensagem}'
-                    
-                    address.send(bytes(mensagem, 'utf-8'))
-                    if mensagem == 'exit':
-                        print('Conexão encerrada')
+
+                if mensagem != "":
+                    mensagem = f"{username}, {mensagem}"
+
+                    address.send(bytes(mensagem, "utf-8"))
+                    if mensagem == "exit":
+                        print("Conexão encerrada")
                     break
             else:
-                print('Username inválido')
-                
-    
-    def escutar_mensagem(self,address):
+                print("Username inválido")
+
+    def escutar_mensagem(self, address):
         while True:
-            recv_msg = address.recv(BUFFER).decode('ascii')
-                
-            if recv_msg != '':
-                if recv_msg == 'exit':
-                    print('Conexão encerrada')
+            recv_msg = address.recv(BUFFER).decode("ascii")
+
+            if recv_msg != "":
+                if recv_msg == "exit":
+                    print("Conexão encerrada")
                     break
                 else:
-                    print(f'Server: {recv_msg}')
+                    print(f"Server: {recv_msg}")
                     break
-        
-    def enviar_e_escutar_mensagem(self,address):
-        
+
+    def enviar_e_escutar_mensagem(self, address):
+
         while True:
             self.enviar_mensagem(address)
             self.escutar_mensagem(address)
-                
+
+
 def start_connection(ip_address):
-    
+
     tcp_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
         destination = (ip_address, SERVER_POST)
         tcp_connection.connect(destination)
+        name = input("Seu username: ")
+        tcp_connection.send(bytes(name, "utf-8"))
     except ConnectionError as error:
-        print('Conexão encerrada\nErro:', error)
-        
+        print("Conexão encerrada\nErro:", error)
+
     return tcp_connection
+
 
 def close_connection(tcp_connection):
     tcp_connection.close()
-    
+
 def conversation(tcp_connection):
-        
-        cliente = Cliente()
-        cliente.enviar_e_escutar_mensagem(tcp_connection)
-               
-        close_connection(tcp_connection)     
-        
-if __name__ == '__main__':
-    
-    print('Conexão iniciada')
+
+    cliente = Cliente()
+    cliente.enviar_e_escutar_mensagem(tcp_connection)
+
+    close_connection(tcp_connection)
+
+
+if __name__ == "__main__":
+
+    print("Conexão iniciada")
     tcp_connection = start_connection(ADDRESS)
     conversation(tcp_connection)
-    
+
     try:
         close_connection(tcp_connection)
     except ConnectionError as error:
-        print('Conexão encerrada\nErro:', error)
+        print("Conexão encerrada\nErro:", error)
--- servidor.py
+++ servidor.py
@@ -15,15 +15,26 @@ import socket
 import threading
 
 
+BUFFER = 1024
+HOST = ""
+PORT = 9000
+PALAVRAS_BANIDA = ["buceta", "caralho", "pika", "rolão"]
+NMR_CLIENTES = 3
+
+
+class Cliente:
+    def __init__(self, client_addr, cliente_socket, user_name) -> None:
+        self.client_addr = client_addr
+        self.cliente_socket = cliente_socket
+        self.user_name = user_name
+
+
 class Servidor:
-    palavras_banidas = ["buceta", "caralho", "pika", "rolão"]
-    host = ""
-    port = 9000
 
     def __init__(self):
-        self.addr = (self.host, self.port)
+        self.addr = (HOST, PORT)
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.clientes = []
+        self.clientes: list[Cliente] = []
         self.banidos = []
 
     def __call__(self):
@@ -33,14 +44,19 @@ class Servidor:
 
         threading.Thread(target=self.thread_connect_users).start()
 
-    def tread_msg_users(self, cliente_socket: socket.socket, client_addr):
+    def tratamento_msg(self, cliente_socket):
+        data = cliente_socket.recv(BUFFER)
+        text_data: str = data.decode()
+        name, msg = text_data.split(", ")
 
+        for cliente in self.clientes:
+            if cliente.user_name == name:
+                cliente.cliente_socket.send(msg.encode())
+
+    def tread_msg_users(self, cliente_socket: socket.socket, client_addr):
         while True:
             try:
-                data = cliente_socket.recv(1024)
-                text_data = data.decode()
-                print(f"{client_addr}: {text_data}")
-                cliente_socket.send("Mensagem Recebida!".encode())
+                self.tratamento_msg(cliente_socket)
             except ConnectionAbortedError:
                 print(f"{client_addr}: Desconectado (abort)")
                 break
@@ -50,16 +66,22 @@ class Servidor:
 
     def thread_connect_users(self):
         while True:
+            log = ""
+
             client_socket, client_addr = self.server_socket.accept()
-            if len(self.clientes) < 10:
-                self.clientes.append(client_socket)
-                print(f"Cliente {client_addr} conectado.")
+            if len(self.clientes) < NMR_CLIENTES:
+                name = client_socket.recv(BUFFER).decode()
+                cliente_atual = Cliente(client_addr, client_socket, name)
+                self.clientes.append(cliente_atual)
+                log = f"Cliente {client_addr} conectado."
                 threading.Thread(
                     target=self.tread_msg_users, args=(client_socket, client_addr)
                 ).start()
             else:
-                print(f"Cliente {client_addr} recusado. Limite de conexões atingido.")
+                log = f"Cliente {client_addr} recusado. Limite de conexões atingido."
                 client_socket.close()
+
+            print(log)
 
 
 if __name__ == "__main__":
